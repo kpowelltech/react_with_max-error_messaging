0557b3b2f6066c2e7fe55946064073a76eb0e235
Keith Powell <keith@tapcart.co>
2022-03-06 22:33:18 -0800

    Created portals for the ErrorModal component, updated README

--- 01-starting-project/src/components/UI/ErrorModal/ErrorModal.js
+++ 01-starting-project/src/components/UI/ErrorModal/ErrorModal.js
@@ -1,27 +1,50 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import styles from "./ErrorModal.module.css";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
+const Backdrop = (props) => {
+  return (
+    <div className={styles.backdrop} onClick={props.onClickErrorModal}></div>
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.errorTitle}</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.errorMessage}</p>
+      </div>
+
+      <footer className={styles.actions}>
+        <Button onClick={props.onClickErrorModal}>OK</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onClickErrorModal}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.errorTitle}</h2>
-        </header>
-
-        <div className={styles.content}>
-          <p>{props.errorMessage}</p>
-        </div>
-
-        <footer className={styles.actions}>
-          <Button onClick={props.onClickErrorModal}>OK</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClickErrorModal={props.onClickErrorModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          errorTitle={props.errorTitle}
+          errorMessage={props.errorMessage}
+          onClickErrorModal={props.onClickErrorModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # react_with_max-error_messaging
 This simple app focuses on React fundamentals and error handling.  
 Concepts explored:
-- CSS modules
-- Ternary operator
+- CSS modules - CSS files in which all class names and animation names are scoped locally by default.
+
+- Ternary operator - The name ternary refers to the fact that the operator takes three operands. The condition is a boolean expression that evaluates to either true or false.
+
 - Passing functions between components
-- Lifting State
+
+- Lifting State up - bubbling State upwards to the closest ancestor of components that need it.  
+
 - Creating Helper wrapping elements
-- props.children
+
+- props.children - a special prop, automatically passed to every component, that can be used to render the content included between the opening and closing tags when invoking a component
+
 - Creating error messaging
+
 - Modal creation
-- React fragments
+
+- React fragments - A common pattern in React is for a component to return multiple elements. Fragments let you group a list of children without adding extra nodes to the DOM
+
+- ReactDOM.createPortal() - rendered html content is moved somewhere else.  
